25c0b20a2f2d1b77f35d34c9a1e979e9c7252903
Malte Tewes <mtewes@astro.uni-bonn.de>
2017-10-04 17:12:29 +0200

    Initial upload of a setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name='f2n',
+	version='2.0dev',
+	packages=['f2n'],
+	description='FITS to net',
+	long_description=open('README.rst').read(),
+	author='Malte Tewes',
+	license='GPLv3',
+	url='http://github.com/mtewes/f2n'
+)
+
