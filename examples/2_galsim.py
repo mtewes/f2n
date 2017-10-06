cdf678e826b501b59f670bfb431232a26455d4a5
Malte Tewes <malte.tewes@gmail.com>
2017-10-06 14:50:12 +0200

    removign old comments

--- examples/2_galsim.py
+++ examples/2_galsim.py
@@ -81,9 +81,7 @@ for row in catalog:
 #print(catalog)
 
 # And we visualize the grid and the measurements:
-
-
-sf = f2n.SimpleFigure(gal_image.array, z1=-3.0, z2=10.0, scale=2)
+sf = f2n.SimpleFigure(gal_image.array, z1=-3.0, z2=10.0, scale=2) # There is no "tranpose" here, as we've defined the stamps with our own convention, and consistently drawn and measured them with galsim.
 sf.draw_g_ellipses(catalog, x="hsm_x", y="hsm_y", g1="hsm_g1", g2="hsm_g2", sigma="hsm_sigma", edgecolor="red")
 sf.annotate(catalog, x="hsm_x", y="hsm_y", text="g1 = {row[hsm_g1]:.2f}", color="white", xytext=(20, -10))
 sf.annotate(catalog, x="hsm_x", y="hsm_y", text="g2 = {row[hsm_g2]:.2f}", color="white", xytext=(20, -25))
@@ -93,25 +91,3 @@ sf.show()
 
 
 
-
-
-
-
-
-"""
-# Read an image as a numpy array
-image_array = f2n.read_fits("example.fits")
-# And generate some tiny catalog (could also be an astropy table)
-catalog = [
-        {"x":100.5, "y":112.0, "g1":0.0, "g2":0.0, "sigma":50.0},
-        {"x":170.0, "y":110.0, "g1":-0.3, "g2":0.0, "sigma":5.0},
-    ]
-
-# Demo of some features using the convenient SimpleFigure class:
-sf = f2n.SimpleFigure(image_array, z1="auto", z2="auto", scale=3)
-sf.draw_g_ellipses(catalog, edgecolor="red") # Further kwargs are passed to the matplotlib Ellipse
-sf.annotate(catalog, text="({row[x]}, {row[y]})", color="white", fontsize=14) # Futher kwargs are passed to matplotib Text
-
-sf.show()
-#sf.save_to_file("test.png")
-"""
